56b54b8ac98d6009310835e5fc6545fc74d9dd3d
JarvisYu <JarvisYu_Pro@outlook.com>
2021-10-20 16:31:58 +0100

    fixes srcAll.sh, can source it now

---
+++ srcAll.sh
@@ -0,0 +1,8 @@
+LAST_PATH=`pwd`
+cd ~/.funcs
+source "./funcs/gitAC.sh"
+source "./funcs/gitCoFet.sh"
+source "./funcs/gitCoFetE.sh"
+source "./funcs/printf.sh"
+source "./funcs/srcZsh.sh"
+cd ${LAST_PATH}
