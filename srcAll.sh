0cc9cf9a7f4ef74ef04e46469c9b6b231244a18e
JarvisYu <JarvisYu_Pro@outlook.com>
2021-10-26 20:57:07 +0100

    updates srcAll, sources files with for loop

--- srcAll.sh
+++ srcAll.sh
@@ -1,15 +1,20 @@
 LAST_PATH=`pwd`
 cd ~/.funcs
-source "./funcs/gitac.sh"
-source "./funcs/gitak.sh"
-source "./funcs/gitcoftch.sh"
-source "./funcs/gitcoftche.sh"
-source "./funcs/gitmvbr.sh"
-source "./funcs/printf.sh"
-source "./funcs/echo.sh"
-source "./funcs/srczsh.sh"
-source "./funcs/srcbsh.sh"
-source "./funcs/status.sh"
+
+for f in ./funcs/*.sh; do
+  source $f
+done
+
+# source "./funcs/gitac.sh"
+# source "./funcs/gitak.sh"
+# source "./funcs/gitcoftch.sh"
+# source "./funcs/gitcoftche.sh"
+# source "./funcs/gitmvbr.sh"
+# source "./funcs/printf.sh"
+# source "./funcs/echo.sh"
+# source "./funcs/srczsh.sh"
+# source "./funcs/srcbsh.sh"
+# source "./funcs/status.sh"
 
 source "./funcs/pintos/gdb_pintos.sh"
 
